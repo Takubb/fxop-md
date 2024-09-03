e20cd3822274beafcf5c2a450d81a8935aa5bc7c
Takubb <takudzwaka1@gmail.com>
2024-09-03 23:03:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const toBool = x => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
- SESSION_ID: process.env.SESSION_ID || "",
+ SESSION_ID: process.env.SESSION_ID || "Session~MkQV8s6x",
  BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLimk
  SUDO: process.env.SUDO || "2348039607375",
  LOGS: toBool(process.env.LOGS) || true,
